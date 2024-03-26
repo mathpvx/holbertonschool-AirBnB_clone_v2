a27e471ab05c7837a7a3a443a3c40a6e42ab4e4d
mathpvx <7547@holbertonstudents.com>
2024-03-26 17:26:58 +0100

    corrected indentation and removed space before method def

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -52,9 +52,9 @@ class FileStorage:
         except FileNotFoundError:
             pass
         
-        def delete (self, obj=None):
-            """deletes obj from __objects if it exists"""
-            if obj is not None:
-                key = "{}.{}".format(obj.__class__.__name__, obj.id)
-                if key in self. __objects:
-                    del self.__objects[key]
+    def delete(self, obj=None):
+        """deletes obj from __objects if it exists"""
+        if obj is not None:
+            key = "{}.{}".format(obj.__class__.__name__, obj.id)
+            if key in self. __objects:
+                del self.__objects[key]
