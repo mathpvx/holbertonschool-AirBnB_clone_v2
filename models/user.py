2cf141119c30cf3896ebd7d298e9c5331deeb770
ken2201 <ndayishimiyeken22@gmail.com>
2024-03-27 16:54:36 +0100

    Update User model with SQLAlchemy support

--- models/user.py
+++ models/user.py
@@ -1,18 +1,32 @@
 #!/usr/bin/python3
-"""This module defines a class User"""
-from models.base_model import BaseModel
+"""module defines a class User"""
+from models.base_model import BaseModel, Base
+from sqlalchemy import DateTime
+from sqlalchemy.sql.schema import Column, ForeignKey
+from sqlalchemy.sql.sqltypes import String, Integer
 
 
+from sqlalchemy.orm import relationship, backref
+from os import getenv
 
-class User(BaseModel):
-    """This class defines a user by various attributes"""
-    __tablename__ = 'users'
-    
-    email = ''
-    password = ''
-    first_name = None
-    last_name = None
 
-    def __init__(self, *args, **kwargs):
-        """Initializes User instance"""
-        super().__init__(*args, **kwargs)
+if getenv('HBNB_TYPE_STORAGE') == 'db':
+    class User(BaseModel, Base):
+        """class defines a user by various attributes"""
+        __tablename__ = 'users'
+        email = Column(String(128), nullable=False)
+        password = Column(String(128), nullable=False)
+        first_name = Column(String(128))
+        last_name = Column(String(128))
+        places = relationship("Place", backref="user",
+                              cascade="all, delete, delete-orphan")
+        reviews = relationship("Review", backref="user",
+                               cascade="all, delete, delete-orphan")
+
+else:
+    class User(BaseModel):
+        """class defines a user by various attributes"""
+        email = ''
+        password = ''
+        first_name = ''
+        last_name = ''
