5109bdefe7fd72df0fe0cb02569d273c11b2476e
ken2201 <ndayishimiyeken22@gmail.com>
2024-03-28 23:25:36 +0100

    Refactor Review model to support database storage

--- models/review.py
+++ models/review.py
@@ -1,10 +1,23 @@
 #!/usr/bin/python3
-""" Review module for the HBNB project """
-from models.base_model import BaseModel
+""" module for the HBNB project """
+from sqlalchemy.sql.schema import Column, ForeignKey, Table
+from sqlalchemy.sql.sqltypes import String, Integer, Float
+
+from models.base_model import BaseModel, Base
+from os import getenv
 
 
-class Review(BaseModel):
-    """ Review classto store review information """
-    place_id = ""
-    user_id = ""
-    text = ""
+if getenv('HBNB_TYPE_STORAGE') == 'db':
+    class Review(BaseModel, Base):
+        """ review class storing review information """
+
+        __tablename__ = "reviews"
+        place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
+        user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+        text = Column(String(1024), nullable=False)
+else:
+    class Review(BaseModel):
+        """ Review class storing review information """
+        place_id = ""
+        user_id = ""
+        text = ""
