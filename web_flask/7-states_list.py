79690420840643c2af156b09655370fea000d17d
mathpvx <7547@holbertonstudents.com>
2024-04-10 10:48:48 +0200

    updates the files

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 """Starts a Flask web application.
 """
-from ..models import storage
-from flask import Flask
-from flask import render_template
+from models import storage
+from models.state import State
+from flask import Flask, render_template
 
 
 app = Flask(__name__)
@@ -15,12 +15,12 @@ def states_list():
 
     States are sorted by name.
     """
-    states = storage.all("State")
+    states = storage.all(State)
     return render_template("7-states_list.html", states=states)
 
 
 @app.teardown_appcontext
-def teardown(exc):
+def teardown(exception=None):
     """Remove the current SQLAlchemy session."""
     storage.close()
 
