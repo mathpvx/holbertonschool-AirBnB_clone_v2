7e5decd8dc69eae6b10dc7e8672d6988a69002b6
mathpvx <7547@holbertonstudents.com>
2024-04-10 09:59:00 +0200

    modified the html

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 """Starts a Flask web application.
 """
-from models import storage
+from ..models import storage
 from flask import Flask
 from flask import render_template
+
 
 app = Flask(__name__)
 
